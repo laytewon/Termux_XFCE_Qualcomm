7fec757a473cbcf86ff3b9b048798c7297557957
bengkelgawai <33578050+bengkelgawai@users.noreply.github.com>
2024-01-17 06:43:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -77,7 +77,7 @@ proot-distro login debian --shared-tmp -- env DISPLAY=:1.0 cp /usr/share/zoneinf
 
 setup_xfce() {
 #Install xfce4 desktop and additional packages
-pkg install git neofetch virglrenderer-android mesa_zink virglrenderer-mesa-zink vulkan-loader-android papirus-icon-theme xfce4 xfce4-goodies pavucontrol-qt exa bat jq nala wmctrl firefox netcat-openbsd -y
+pkg install git neofetch virglrenderer-android mesa-zink virglrenderer-mesa-zink vulkan-loader-android papirus-icon-theme xfce4 xfce4-goodies pavucontrol-qt exa bat jq nala wmctrl firefox netcat-openbsd -y
 
 #Create .bashrc
 cp $HOME/../usr/var/lib/proot-distro/installed-rootfs/debian/etc/skel/.bashrc $HOME/.bashrc
